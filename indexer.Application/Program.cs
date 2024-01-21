79e345bd6168a90ff0b87de6223bfefa5e5c5b80
Brian Lake <bnlake@gmail.com>
2024-01-21 10:32:33 -0600

    utilize serilog

--- indexer.Application/Program.cs
+++ indexer.Application/Program.cs
@@ -1,10 +1,15 @@
 using indexer.Application;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddIndexerApplication();
+    })
+    .UseSerilog((context, configuration) =>
+    {
+        configuration.ReadFrom.Configuration(context.Configuration).WriteTo.Console();
     })
     .Build();
 
