7e1db084dd6fb5c147d964d7dc46fb6c48557eb0
Brian Lake <blake@webmd.net>
2024-01-24 21:19:44 -0600

    create docker compose and seed script

--- .init/init-db.sql
+++ .init/init-db.sql
@@ -1,3 +1,10 @@
-CREATE SCHEMA UCR;
-CREATE USER localdev WITH PASSWORD 'localDev';
-GRANT ALL PRIVILEGES ON SCHEMA UCR TO localdev;
+CREATE TABLE "Contents" (
+    "Id" SERIAL PRIMARY KEY,
+    "Name" VARCHAR(255) NOT NULL
+);
+
+INSERT INTO "Contents" ("Name")
+VALUES
+    ('Symptoms of a Heart Attack'),
+    ('Coping with Headaches'),
+    ('Pregnancy and Your Diet');
