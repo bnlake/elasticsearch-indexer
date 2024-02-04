81c6fdb66d9a449562a8ed1bae6a040720374c4e
Brian Lake <blake@webmd.net>
2024-02-03 19:12:20 -0600

    load env vars with prefix

--- indexer.Application/Program.cs
+++ indexer.Application/Program.cs
@@ -1,7 +1,10 @@
 using indexer.Application;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 var builder = Host.CreateApplicationBuilder();
+
+builder.Configuration.AddEnvironmentVariables(prefix: "Indexer__");
 
 builder.Services.AddIndexerApplication(builder.Configuration);
 builder.Services.AddSerilog(config => config.ReadFrom.Configuration(builder.Configuration).WriteTo.Console());
